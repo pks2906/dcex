aba5c4e73a3e7b4a21d0cdda6a08d0f4dcad0bcc
pks2906 <pratik1971sinha@gmail.com>
2024-09-19 18:31:51 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
       </main>
   
   );
