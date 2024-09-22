bf224887d56556162f11733ed104a2431a59fc15
pks2906 <pratik1971sinha@gmail.com>
2024-09-22 20:19:43 +0530

    oauth error solved

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,3 +8,18 @@ export const PrimaryButton = ({children, onClick}: {
     </button>
         
 }
+
+export const SecondaryButton = ({children, onClick, prefix}: {
+    children: React.ReactNode,
+    onClick: () => void,
+    prefix?: React.ReactNode
+}) => {
+    return <button onClick={onClick} type="button" className="text-white bg-blue-500 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-6 py-3 me-2 mb-2 flex">
+    <div>
+        {prefix}
+    </div>
+    <div>
+        {children}
+    </div>
+</button>
+}
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { signIn } from "next-auth/react"
+import { SecondaryButton } from "./Button"
 
 export const Hero = () => {
     return <div>
@@ -8,6 +11,18 @@ export const Hero = () => {
             <span className="text-blue-500 pl-4">
                 Revolution
             </span>
+        </div>
+        <div className="flex justify-center pt-4 text-2xl text-slate-500">
+            Create a frictionless wallet from India with just a Google Account.
+            
+        </div>
+        <div className="flex justify-center pt-2 text-2xl text-slate-500">
+            Convert your INR into Cryptocurrency
+        </div>
+        <div className="pt-8 flex justify-center">
+            <SecondaryButton onClick={() => {
+                signIn("google");
+            }}>Login with Google</SecondaryButton>
         </div>
     </div>
 }
