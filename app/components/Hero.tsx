78f93cd889ad7e03ec65fe80bf2c8fb1278db9b7
pks2906 <pratik1971sinha@gmail.com>
2024-09-22 19:46:06 +0530

    landing page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,13 @@
 
 export const Hero = () => {
     return <div>
-        <div className="text-4xl font-extrabold">
-            The Indian Crypto Exchange Revolution
+        <div className="text-5xl font-medium">
+            <span>
+                The Indian Crypto Exchange 
+            </span>
+            <span className="text-blue-500 pl-4">
+                Revolution
+            </span>
         </div>
     </div>
 }
