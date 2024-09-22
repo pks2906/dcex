8166ebadfaee895787a4397c0a7a338bbccc0a5f
pks2906 <pratik1971sinha@gmail.com>
2024-09-22 12:12:51 +0530

    added styles

---
+++ app/components/Button.tsx
@@ -0,0 +1,10 @@
+"use client";
+export const PrimaryButton = ({children, onClick}: {
+    children: React.ReactNode,
+    onClick: () => void
+}) => {
+    return <button onClick={onClick} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+        {children}
+    </button>
+        
+}
---
+++ app/components/Hero.tsx
@@ -0,0 +1,8 @@
+
+export const Hero = () => {
+    return <div>
+        <div className="text-4xl font-extrabold">
+            The Indian Crypto Exchange Revolution
+        </div>
+    </div>
+}
