79781395b2b5aa360d7818682481fc80621e6fa5
pks2906 <pratik1971sinha@gmail.com>
2024-09-23 18:31:09 +0530

    schema creation done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import GoogleProvider from "next-auth/providers/google";
-
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+import db from "@/app/db";
+import { Keypair } from "@solana/web3.js";
 
 
 //signup/signin with google
@@ -20,8 +21,43 @@ const handler = NextAuth({
           return false
         }
 
-        
-        
+        const userDb = await db.user.findFirst({
+          where: {
+            username: email
+
+          }
+
+        })
+
+        if (userDb){
+          return true;
+        }
+
+        const keypair = Keypair.generate();
+        const publicKey = keypair.publicKey.toBase58(); 
+        const privateKey = keypair.secretKey;
+        console.log(publicKey)
+        console.log(privateKey)
+        await db.user.create({
+          data: {
+            username: email,
+            provider: "Google",
+            solWallet: {
+              create: {
+                publicKey: publicKey,
+                privateKey: privateKey.toString()
+
+              }
+            },
+            inrWallet: {
+              create: {
+                balance: 0
+              }
+            }
+          }
+        })
+
+        return true;
 
       }
       
