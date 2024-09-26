98e0978f744198df2fc2291a57fee34c7d0aebec
pks2906 <pratik1971sinha@gmail.com>
2024-09-27 00:11:24 +0530

    swap init

--- app/components/Swap.tsx
+++ app/components/Swap.tsx
@@ -13,6 +13,9 @@ export function Swap({ publicKey, tokenBalances }: {
 }) {
     const [baseAsset, setBaseAsset] = useState(SUPPORTED_TOKENS[0])
     const [quoteAsset, setQuoteAsset] = useState(SUPPORTED_TOKENS[1])
+    const [baseAmount, setBaseAmount] = useState(0);
+    const [quoteAmount, setQuoteAmount] = useState(0);
+
 
     return <div className="p-12">
         <div className="text-2xl font-bold pb-4">
@@ -21,7 +24,13 @@ export function Swap({ publicKey, tokenBalances }: {
         <SwapInputRow onSelect={(asset) => {
             setBaseAsset(asset)
         }} selectedToken={baseAsset} title={"You pay:   "} topBorderEnabled={true} bottomBorderEnabled={false}
-        subtitle={<div className="text-slate-500 pt-1 text-sm pl-1">Current Balance: {tokenBalances?.tokens.find(x => x.name === baseAsset.name)?.balance} {baseAsset.name}</div>} />
+            subtitle={<div className="text-slate-500 pt-1 text-sm pl-1 flex">
+                <div className="font-normal pr-1">
+                    Current Balance:
+                </div>
+                <div className="font-semibold">{tokenBalances?.tokens.find(x => x.name === baseAsset.name)?.balance} {baseAsset.name}
+                </div>
+            </div>} />
         <div className="flex justify-center">
             <div onClick={() => {
                 let baseAssestTemp = baseAsset;
@@ -29,7 +38,7 @@ export function Swap({ publicKey, tokenBalances }: {
                 setQuoteAsset(baseAssestTemp);
             }} className="cursor-pointer rounded-full w-10 h-10 border absolute mt-[-20px] bg-white flex justify-center pt-2">
                 <SwapIcon />
-           
+
 
 
             </div>
@@ -51,7 +60,7 @@ function SwapInputRow({ onSelect, selectedToken, title, subtitle, topBorderEnabl
     topBorderEnabled: boolean;
     bottomBorderEnabled: boolean;
 }) {
-    return <div className={`border flex justify-between p-4 ${topBorderEnabled ? "rounded-t-xl" : ""} ${bottomBorderEnabled ?
+    return <div className={`border flex justify-between p-6 ${topBorderEnabled ? "rounded-t-xl" : ""} ${bottomBorderEnabled ?
         "rounded-b-xl" : ""} `}>
         <div>
             <div className="text-xs font-semibold mb-1">
@@ -59,6 +68,9 @@ function SwapInputRow({ onSelect, selectedToken, title, subtitle, topBorderEnabl
             </div>
             <AssestSelector selectedToken={selectedToken} onSelect={onSelect} />
             {subtitle}
+        </div>
+        <div>
+            <input placeholder="0" type="text" className="p-6 outline-none text-4xl" dir="rtl"></input>
         </div>
     </div>
 }
@@ -68,7 +80,7 @@ function AssestSelector({ selectedToken, onSelect }: {
     onSelect: (asset: TokenDetails) => void;
 
 }) {
-    return <div>
+    return <div className="w-24">
         <select onChange={(e) => {
             const selectedToken = SUPPORTED_TOKENS.find(x => x.name === e.target.value);
             if (selectedToken) {
@@ -83,10 +95,12 @@ function AssestSelector({ selectedToken, onSelect }: {
         </select>
     </div>
 
-}   
+}
 
-function SwapIcon(){
-    return  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+function SwapIcon() {
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
     </svg>
 }
+
+//4:8:36
